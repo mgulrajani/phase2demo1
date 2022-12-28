42f045ad774be35680e37dd10a77554a00b0ea5d
mgulrajani <muskaangulrajani@hotmail.com>
2022-12-28 10:10:17 +0530

    welcome component test

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,4 +1,7 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RepeatPipe } from '../repeat.pipe';
 
 import { WelcomeComponent } from './welcome.component';
 
@@ -8,7 +11,8 @@ describe('WelcomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ]
+      declarations: [ WelcomeComponent ,
+      RepeatPipe]
     })
     .compileComponents();
 
@@ -20,4 +24,27 @@ describe('WelcomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display data as 10',()=>{
+ //original value in the component is 3
+ //here we are assigning 10 ,after obtaining the instance
+
+    component.data =10;
+
+    //triggers change detection cycle of angular
+    fixture.detectChanges();
+
+    //Obtaining the root element of the dom
+    const rootEle:DebugElement = fixture.debugElement;
+    //accessing the h1 from the dom
+    //and storing it in h1
+    const h1 =rootEle.query(By.css('#msg'));
+
+    const h1Element:HTMLElement= h1.nativeElement;
+
+    expect(h1Element.textContent).toContain('10');
+
+
+
+  })
 });
