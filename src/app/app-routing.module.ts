8665a0776886d153b0dfffaf606bb3e3e02d516f
mgulrajani <muskaangulrajani@hotmail.com>
2022-12-30 12:48:33 +0530

    routing basic demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,65 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { AppComponent } from './app.component';
 import { EventDetailComponent } from './events/event-detail.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ProductAddComponent } from './products/product-add.component';
+import { StudentAddComponent } from './student/student-add.component';
+import { CreateTrusteeComponent } from './trustees/create-trustee.component';
+import { EditTrusteeComponent } from './trustees/edit-trustee.component';
+import { TrusteeListComponent } from './trustees/trustee-list.component';
+import { ViewTrusteeComponent } from './trustees/view-trustee.component';
 
 //created  a Routes array
 //Routes
 //[ { path , component},
 //{  path , component , } ]
-const routes: Routes = [
+/* const routes: Routes = [
+  {
+    path: '',
+    component: TrusteeListComponent
+  },
+  {
+    path: 'trustees/:id/view',
+    component: ViewTrusteeComponent
+  },
+  {
+    path: 'trustees/:id/edit',
+    component: EditTrusteeComponent
+  },
+  {
+    path: 'trustees/new',
+    component: CreateTrusteeComponent
+  },
+  {
+    path: '**',
+    component: TrusteeListComponent
+  }
+]; *//*
   {path:'events/:id',component:EventDetailComponent},
   {path:'addProduct',component:ProductAddComponent},
   {path:'',pathMatch:'full' ,component:AppComponent}]
+
+
+*/
+const routes:Routes=[
+
+  {
+
+path:'',component:HomeComponent,pathMatch:'full',
+
+},
+{
+  path:'about-us',component:AboutUsComponent
+
+},
+{
+path:'student',component:StudentAddComponent
+},
+{path:'**',component:PagenotfoundComponent}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
