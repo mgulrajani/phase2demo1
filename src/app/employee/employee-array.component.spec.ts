908f9e7823bbabcccfbd41ac2a1691d81bf997d9
mgulrajani <muskaangulrajani@hotmail.com>
2022-12-30 09:56:11 +0530

    form array example

---
+++ src/app/employee/employee-array.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeeArrayComponent } from './employee-array.component';
+
+describe('EmployeeArrayComponent', () => {
+  let component: EmployeeArrayComponent;
+  let fixture: ComponentFixture<EmployeeArrayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmployeeArrayComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EmployeeArrayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/employee/employee-array.component.ts
@@ -0,0 +1,66 @@
+import { FormatWidth } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee-array',
+  templateUrl: './employee-array.component.html',
+  styleUrls: ['./employee-array.component.css']
+})
+export class EmployeeArrayComponent implements OnInit {
+   empForm!:FormGroup;
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.empForm = this.fb.group({
+      employees:this.fb.array([])}
+    );
+  }
+
+  employees():FormArray{
+    return this.empForm.get("employees") as FormArray;
+  }
+
+  newEmployee():FormGroup{
+    return this.fb.group({
+         firstName:'',
+         lastName:'',
+         skills:this.fb.array([])
+    });
+
+  }
+
+  addEmployee(){
+    this.employees().push(this.newEmployee());
+  }
+
+  removeEmployee(empIndex:number){
+    this.employees().removeAt(empIndex);
+  }
+
+  employeeSkills(empIndex:number):FormArray{
+    return this.employees().at(empIndex).get("skills") as FormArray;
+  }
+
+
+newSkill():FormGroup{
+  return this.fb.group({
+    skill:'',
+    exp:''
+  });
+}
+  addEmployeeSkill(empIndex:number){
+    this.employeeSkills(empIndex).push(this.newSkill());
+
+
+}
+removeEmployeeSkill(empIndex:number,skillIndex:number){
+  this.employeeSkills(empIndex).removeAt(skillIndex);
+}
+
+onSubmit(){
+  console.log(this.empForm.value);
+  
+}
+}
